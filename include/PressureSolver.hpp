61e527070b537d97da213e8c27765da2e63e9722
Iciar Arnal <iciar.arnal-tribaldos@tum.de>
2026-04-29 11:31:42 +0200

    Implement SOR-Iciar pressure solver

--- include/PressureSolver.hpp
+++ include/PressureSolver.hpp
@@ -55,3 +55,29 @@ class SOR : public PressureSolver {
   private:
     double _omega;
 };
+
+class SOR_Iciar : public PressureSolver {
+  public:
+    SOR() = default;
+
+    /**
+     * @brief Constructor of SOR solver
+     *
+     * @param[in] relaxation factor
+     */
+    SOR(double omega);
+
+    virtual ~SOR() = default;
+
+    /**
+     * @brief Solve the pressure equation on given field, grid and boundary
+     *
+     * @param[in] field to be used
+     * @param[in] grid to be used
+     * @param[in] boundary to be used
+     */
+    virtual double solve(Fields &field, Grid &grid, const std::vector<std::unique_ptr<Boundary>> &boundaries);
+
+  private:
+    double _omega;
+};
--- src/PressureSolver.cpp
+++ src/PressureSolver.cpp
@@ -3,6 +3,7 @@
 #include "Communication.hpp"
 #include "PressureSolver.hpp"
 
+// THOMAS IMPLEMENTATION OF 'PressureSolver.cpp'
 SOR::SOR(double omega) : _omega(omega) {}
 
 double SOR::solve(Fields &field, Grid &grid, const std::vector<std::unique_ptr<Boundary>> & /*boundaries*/) {
@@ -45,3 +46,44 @@ double SOR::solve(Fields &field, Grid &grid, const std::vector<std::unique_ptr<B
     }
     return std::sqrt(rloc / static_cast<double>(N > 0 ? N : 1));
 }
+
+// ICIAR'S IMPLEMENTATION OF 'PressureSolver.cpp'
+SOR::SOR_Iciar(double omega) : _omega(omega) {}
+
+double SOR_Iciar::solve(Fields &field, Grid &grid, const std::vector<std::unique_ptr<Boundary>> &boundaries) {
+
+    double dx = grid.dx();
+    double dy = grid.dy();
+
+    // Pre-computed coefficient for the update formula
+    double coeff = _omega / (2.0 * (1.0 / (dx * dx) + 1.0 / (dy * dy))); // = _omega * h^2 / 4.0, if dx == dy == h
+    
+    // Iteration of the SOR going from it to it+1
+    // Loop over all fluid cells to compute p(i,j) at it+1 from it values
+    for (auto currentCell : grid.fluid_cells()) {
+        int i = currentCell->i();
+        int j = currentCell->j();
+
+        field.p(i, j) = (1.0 - _omega) * field.p(i, j) +
+                        coeff * (Discretization::sor_helper(field.p_matrix(), i, j) - field.rs(i, j));
+    }
+
+    double res = 0.0;   // residual initialization
+    double rloc = 0.0;  // accumulates val^2 for every cell
+
+    // Cummulative sum of squared residuals (Laplacian(p(i,j)) - rhs(i,j))^2 over all cells
+    for (auto currentCell : grid.fluid_cells()) {
+        int i = currentCell->i();
+        int j = currentCell->j();
+
+        double val = Discretization::laplacian(field.p_matrix(), i, j) - field.rs(i, j);
+        rloc += (val * val);
+    }
+    {
+        res = rloc / (grid.fluid_cells().size()); // mean of squared residuals
+        res = std::sqrt(res);                     // L2 norm of the residual
+    }
+
+    return res;
+}
+
