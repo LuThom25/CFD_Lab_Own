15e0f27d64b8def3e5ec8f3d30d48d5516d5abbe
Iciar Arnal <iciar.arnal-tribaldos@tum.de>
2026-05-29 17:50:08 +0200

    Task 1 from WS3. MPI initialization and finalization, enabling or disabling parallel run (MPI communication) and added the checking for user input

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,32 @@
+#include <mpi.h>
 #include <iostream>
 #include <string>
 
 #include "Case.hpp"
 
 int main(int argn, char **args) {
-
+    // Initialize MPI: assign rank numbers and define global communicator
+    MPI_Init(&argn, &args); 
+    
+    // Get the number of processes in the communicator
+    // -> MPI_COMM_WORLD is the global communicator comprising all processes 
+    int size; // total number of processes, each of which owns one domain
+    MPI_Comm_size(MPI_COMM_WORLD, &size); 
+    
+    // Get the assigned rank number of the process
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     if (argn > 1) {
         std::string file_name{args[1]};
-        Case problem(file_name, argn, args);
+        // Need to pass to the constructor: size and my_rank, to know which 
+        // subdomain it owns and who its neighbohrs are
+        Case problem(file_name, argn, args, size, rank);
         problem.simulate();
 
     } else {
         std::cout << "Error: No input file is provided to fluidchen." << std::endl;
         std::cout << "Example usage: /path/to/fluidchen /path/to/input_data.dat" << std::endl;
     }
+    // Wait for all processes to exit
+    MPI_Finalize();
 }
